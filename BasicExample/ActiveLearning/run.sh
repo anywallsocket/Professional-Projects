a15b760990143046c825cc04a9f94b04fbbfa1b3
anywallsocket <98923858+anywallsocket@users.noreply.github.com>
2025-09-16 20:20:23 -0400

    Update run.sh

--- BasicExample/ActiveLearning/run.sh
+++ BasicExample/ActiveLearning/run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# ALA sample script
+# note that one can optimize NNs
+# for clusters or adsorption sites
+
 START=0
 LOOPS=10
 GAs=10
@@ -15,6 +19,7 @@ do
 	for ((J=0; J<$GAs; J++));
 	do
 		python3 ads.py $I $J
+  		#python3 clust.py $I $J
 		wait
 	done
 
